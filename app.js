3a6a32c699c7fa7bb97e2da24ebf6b8e27adee14
Raselwebdesigner <53082948+Raselwebdesigner@users.noreply.github.com>
2021-01-01 09:59:15 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', (e) => {
+	cursor.style.top = (e.pageY) + "px";
+	cursor.style.left = (e.pageX) + "px";
+});
